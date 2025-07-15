1ba0c9d2f9751fbf97153371fc91fc2828b42ff1
dharani-eswaramurthi <dharani96556@gmail.com>
2025-07-15 15:17:22 +0530

    Initial push

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,13 +2,13 @@ module.exports = {
   apps: [
     {
       name: "backend",
-      script: "uvicorn main:app --host 0.0.0.0 --port 8000",
+      script: "uvicorn mcp_server:app --host 0.0.0.0 --port 8000",
       interpreter: "python3"
     },
     {
       name: "frontend",
-      script: "streamlit run frontend_app.py --server.port=8501",
-      interpreter: "bash"
+      script: "streamlit run ui_new.py",
+      interpreter: "python3",
     }
   ]
 }
